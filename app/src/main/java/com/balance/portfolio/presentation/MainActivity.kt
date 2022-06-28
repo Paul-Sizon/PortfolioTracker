be7a7c132ce0ef74a9e201e346842dbfd03870c5
psizonenko <psizonenko@griddynamics.com>
2022-06-28 21:36:53 +0400

    mock coin list created

--- app/src/main/java/com/balance/portfolio/presentation/MainActivity.kt
+++ app/src/main/java/com/balance/portfolio/presentation/MainActivity.kt
@@ -1,43 +1,65 @@
-package com.balance.portfolio
+package com.balance.portfolio.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.balance.portfolio.ui.theme.PortfolioTheme
+import androidx.compose.ui.unit.dp
+import com.balance.portfolio.domain.model.Coin
+import com.balance.portfolio.theme.PortfolioTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PortfolioTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Portfolio()
                 }
             }
         }
     }
 }
 
+val mock_coins = listOf<Coin>(Coin("btc", 5.0), Coin("eth", 0.2))
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Portfolio() {
+    LazyColumn() {
+        items(mock_coins) { coin ->
+            Element(coin = coin)
+            Spacer(modifier = Modifier.padding(bottom = 4.dp))
+        }
+    }
+}
+
+@Composable
+fun Element(coin: Coin) {
+    Row() {
+        Text(text = coin.name)
+        Spacer(modifier = Modifier.padding(horizontal = 30.dp))
+        Text(text = coin.price.toString())
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     PortfolioTheme {
-        Greeting("Android")
+        Portfolio()
     }
 }
