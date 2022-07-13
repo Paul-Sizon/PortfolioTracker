f0da89beef4b53ad0516862aa81f48145340bf2c
psizonenko <psizonenko@griddynamics.com>
2022-07-13 18:19:26 +0400

    add navhost

--- app/src/main/java/com/balance/portfolio/presentation/MainActivity.kt
+++ app/src/main/java/com/balance/portfolio/presentation/MainActivity.kt
@@ -7,6 +7,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.balance.portfolio.theme.PortfolioTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -16,6 +19,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PortfolioTheme {
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = "MainScreen"
+                ){
+                    composable("MainScreen") { MainScreen() }
+                }
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
