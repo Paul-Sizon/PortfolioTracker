18917fe7d58106f529f65502ff0905b5e51ffff2
psizonenko <psizonenko@griddynamics.com>
2023-01-13 17:40:53 +0300

    secured api key

---
+++ app/src/main/java/com/balance/portfolio/data/Keys.kt
@@ -0,0 +1,7 @@
+package com.balance.portfolio.data
+
+import com.balance.portfolio.BuildConfig
+
+
+//todo use another in production
+const val api_key = BuildConfig.API_KEY
--- app/src/main/java/com/balance/portfolio/data/remote/CoinApi.kt
+++ app/src/main/java/com/balance/portfolio/data/remote/CoinApi.kt
@@ -1,5 +1,6 @@
 package com.balance.portfolio.data.remote
 
+import com.balance.portfolio.data.api_key
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.Path
@@ -7,14 +8,11 @@ import retrofit2.http.Path
 interface CoinApi {
 
     @GET("v1/exchangerate/{coinId}/USD/")
-    @Headers("X-CoinAPI-Key: $apiKey")
+    @Headers("X-CoinAPI-Key: $api_key")
     suspend fun getCoinById(@Path("coinId") coinId: String): CoinDto
 
     companion object {
         const val BASE_URL = "https://rest.coinapi.io/"
-
-        //todo use another in production
-        const val apiKey = "4F635389-87E1-431E-B0F7-0678EA8DDCCB"
     }
 
 }
